1bb4b8bad8cda6a69bbd136255eb66ec52bf11e4
e_rodrigues <erodrigu@enssat.fr>
2024-03-19 15:18:13 +0100

    New structure of projet for proper web integration

--- src/main/java/fr/enssat/batman/AppConfig.java
+++ src/main/java/fr/enssat/batman/AppConfig.java
@@ -2,7 +2,7 @@ package fr.enssat.batman;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-
+// http://localhost:8080/Dj_Batman/api/
 @ApplicationPath("/api")
 public class AppConfig extends Application {
 }
--- src/main/java/fr/enssat/batman/DjController.java
+++ src/main/java/fr/enssat/batman/DjController.java
@@ -5,6 +5,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+
 @Path("/dj-management")
 public class DjController {
 	
