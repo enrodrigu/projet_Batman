d461124878ae9bfa4a58b190a505668aadc8ae99
e_rodrigues <erodrigu@enssat.fr>
2024-03-19 10:54:24 +0100

    Initial commit

---
+++ src/main/java/fr/enssat/batman/DjController.java
@@ -0,0 +1,18 @@
+package fr.enssat.batman;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/dj-management")
+public class DjController {
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/events")
+	public String events() {
+		return "hello world!";
+	}
+}
