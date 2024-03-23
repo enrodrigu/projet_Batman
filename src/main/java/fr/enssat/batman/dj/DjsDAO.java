2f056946b4bd4d1c5871fa0a6311fcde704ff131
p_scheller <pschelle@enssat.fr>
2024-03-23 17:35:31 +0100

    DAO DJ

---
+++ src/main/java/fr/enssat/batman/dj/DjsDAO.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DjsDAO {
+
+    // Les détails de la connexion à la base de données
+    private static final String URL = "jdbc:mysql://vps817240.ovh.net:3306/info_batman_schema";
+    private static final String USERNAME = "info_batman";
+    private static final String PASSWORD = "811p!er&Nm6";
+
+    // Requêtes SQL
+    private static final String SELECT_ALL_DJs = "SELECT * FROM DJ";
+    private static final String INSERT_DJ = "INSERT INTO DJ (nom, prenom, nom_scene, date_naissance, lieu_residence, style_musical) VALUES (?, ?, ?, ?, ?, ?)";
+    private static final String EDIT_DJ_BY_ID = "UPDATE DJ SET nom = ?, prenom = ?, nom_scene = ?, date_naissance = ?, lieu_residence = ?, style_musical = ? WHERE nom_scene = ?";
+    private static final String DELETE_DJ_BY_ID = "DELETE FROM DJ WHERE nom_scene = ?";
+
+    // Méthode pour récupérer tous les Djs
+    public List<Dj> getAllUsers() {
+        List<Dj> djs = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_DJs);
+            ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                String nom = resultSet.getString("nom");
+                String prenom = resultSet.getString("prenom");
+                String nom_scene = resultSet.getString("nom_scene");
+                String date_naissance = resultSet.getString("date_naissance");
+                String lieu_residence = resultSet.getString("lieu_residence");
+                String style_musical = resultSet.getString("style_musical");
+                djs.add(new Dj(nom, prenom, nom_scene, date_naissance, lieu_residence, style_musical));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return djs;
+    }
+
+    // Méthode pour insérer un nouvel Dj
+    public void addDj(Dj dj) {
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_DJ)) {
+            preparedStatement.setString(1, dj.getNom());
+            preparedStatement.setString(2, dj.getPrenom());
+            preparedStatement.setString(2, dj.getNom_scene());
+            preparedStatement.setString(2, dj.getDate_naissance());
+            preparedStatement.setString(2, dj.getLieu_residence());
+            preparedStatement.setString(2, dj.getStyle_musical());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
